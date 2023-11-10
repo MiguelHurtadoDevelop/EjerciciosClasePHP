84687fc0a31bb1d3b1259d6014acd47d8f3f51fa
MiguelHurtado <mhurtadojunior@gmail.com>
2023-11-10 14:26:37 +0100

    Monedero Terminado

--- Monedero/Config/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-    define('CONTROLLER_DEFAULT', 'MonederoController');
-    define('ACTION_DEFAULT', 'mostrarMonedero');
-    define('URL_DEFAULT', 'http://localhost/EjerciciosClasePHP/Monedero/');
--- Monedero/Views/Layout/footer.php
+++
@@ -1,6 +0,0 @@
-<footer>
-
-    <h3>Creado por Miguel Hurtado &copy;</h3>
-</footer>
-</body>
-</html>
